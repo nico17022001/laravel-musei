a9158eb39321dfaa7d5e4d6a5df551ffe05795e3
nico17022001 <nico17022001boolean@gmail.com>
2023-06-14 17:03:28 +0200

    aggiunto home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.main')
+
+@section('content')
+
+    <div class="n-container d-flex justify-content-center flex-column align-items-center">
+        <h1>Accedi al tuo account</h1>
+
+        <button type="button" class="btn btn-primary">Accedi</button>
+    </div>
+
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,18 @@
+    <div class="nd-container">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-2">
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Musei</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Artisti</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+
